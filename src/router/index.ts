e0f6146959ac7eb24e3de71de795f81e3d93d67a
ataola <zjt613@gmail.com>
2022-01-22 00:35:09 +0800

    ci: update config

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: () => import('@/views/Home.vue'),
+    meta: {
+      title: '首页'
+    }
+  }
+]
 
 const router = createRouter({
   history: createWebHistory('/'),
-  routes: []
+  routes
 })
 
 export default router
