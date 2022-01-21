e311f222d8092db8813e142c91a73a9e02245275
ataola <zjt613@gmail.com>
2022-01-21 23:39:49 +0800

    ci: update config

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,32 +2,32 @@ import Axios from 'axios'
 
 const baseURL = '#'
 const request = Axios.create({
-    baseURL,
-    timeout: 20000
+  baseURL,
+  timeout: 20000
 })
 
 request.interceptors.request.use(
     (response) => {
-        return response
+      return response
     },
     (error) => {
-        return Promise.reject(error)
+      return Promise.reject(error)
     }
 )
 
 request.interceptors.response.use(
     (response) => {
-        return response
+      return response
     },
     (error) => {
-        if (error.response && error.response.data) {
-            const code = error.response.status
-            const msg = error.response.data.message
-            console.error(`[Axios Error]`, error.response)
-        } else {
-            alert(`${error}`)
-        }
-        return Promise.reject(error)
+      if (error.response && error.response.data) {
+        const code = error.response.status
+        const msg = error.response.data.message
+        console.error(`[Axios Error]`, error.response)
+      } else {
+        alert(`${ error }`)
+      }
+      return Promise.reject(error)
     }
 )
 
