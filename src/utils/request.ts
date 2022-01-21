e90ce5c8359b49a185813344560f78ed153b9887
ataola <zjt613@gmail.com>
2022-01-21 20:47:57 +0800

    feature: init project

---
+++ src/utils/request.ts
@@ -0,0 +1,34 @@
+import Axios from 'axios'
+
+const baseURL = '#'
+const request = Axios.create({
+    baseURL,
+    timeout: 20000
+})
+
+request.interceptors.request.use(
+    (response) => {
+        return response
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
+request.interceptors.response.use(
+    (response) => {
+        return response
+    },
+    (error) => {
+        if (error.response && error.response.data) {
+            const code = error.response.status
+            const msg = error.response.data.message
+            console.error(`[Axios Error]`, error.response)
+        } else {
+            alert(`${error}`)
+        }
+        return Promise.reject(error)
+    }
+)
+
+export default request
