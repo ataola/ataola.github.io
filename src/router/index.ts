5c1527335e102484dc4dc8b322c4e6dc7768bd24
ataola <zjt613@gmail.com>
2022-01-22 15:36:48 +0800

    ci: update config

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,7 +16,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/'),
+  history: createWebHistory('https://zhengjiangtao.cn'),
   routes
 })
 
