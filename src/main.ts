04cedce619ec77318f6c0777d0bd77d1658f1153
ataola <zjt613@gmail.com>
2022-01-26 00:26:53 +0800

    feature: finish bootstrap page

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from '@/router/index'
 import App from '@/App.vue'
+import 'normalize.css/normalize.css'
+
 
 const app = createApp(App)
 app.use(createPinia())
---
+++ src/views/bootstrap.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="cover">
+    <span class="overlay"></span>
+    <div class="logo">
+      <h2><img class="avatar" src="../static/brands/zheng.png" alt="郑"/>
+        <span class="blink">{{ title }}</span>
+      </h2>
+      <router-link :to="{ name: 'Home' }">{{ tip }}&nbsp;&nbsp;&nbsp;&nbsp;➞</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'Bootstrap',
+  setup(props, {emit, slots, attrs}) {
+    const tip = ref('点此进入')
+    const title = ref('江涛的个人网站')
+
+    return {
+      tip,
+      title
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.cover {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  position: relative;
+  overflow: hidden;
+  box-sizing: border-box;
+  background-image: url('../static/cover/bg.jpg');
+  background-repeat: no-repeat;
+  background-position: center;
+
+  .overlay {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    overflow: hidden;
+    z-index: 6;
+    opacity: 0.4;
+    background-color: #fff;
+  }
+
+  .logo {
+    min-width: 2rem;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 7;
+    overflow: hidden;
+    text-align: center;
+    transition: all 0.2s linear;
+
+    h2 {
+      display: flex;
+      align-items: center;
+      font-size: 0.32rem;
+      cursor: default;
+      animation: down linear 1s;
+
+      .avatar {
+        width: .42rem;
+        height: .42rem;
+      }
+    }
+
+    a {
+      display: block;
+      color: #fff;
+      opacity: 0.7;
+      font-size: 0.24rem;
+      text-decoration: none;
+      text-shadow: 2px 2px 2px #000000;
+      transition: all 0.2s linear;
+      animation: up linear 1s;
+    }
+
+    a:hover {
+      color: #000;
+    }
+  }
+}
+
+@keyframes up {
+  0% {
+    transform: translateY(200%);
+  }
+  100% {
+    transform: translateY(0%);
+  }
+}
+
+@-webkit-keyframes up {
+  0% {
+    transform: translateY(200%);
+  }
+  100% {
+    transform: translateY(0%);
+  }
+}
+
+@keyframes down {
+  0% {
+    transform: translateY(-200%);
+  }
+  100% {
+    transform: translateY(0%);
+  }
+}
+
+@-webkit-keyframes down {
+  0% {
+    transform: translateY(-200%);
+  }
+  100% {
+    transform: translateY(0%);
+  }
+}
+
+.blink {
+  background: -webkit-linear-gradient(left, #fff, #f00 6.25%, #ff7d00 12.5%, #ff0 18.75%, #0f0 25%, #0ff 31.25%, #00f 37.5%, #f0f 43.75%, #ff0 50%, #f00 56.25%, #ff7d00 62.5%, #ff0 68.75%, #0f0 75%, #0ff 81.25%, #00f 87.5%, #f0f 93.75%, #ff0);
+  background-size: 200% 100%;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  animation: masked-animation 2s linear infinite;
+}
+
+@keyframes masked-animation {
+  0% {
+    background-position: 0 0;
+  }
+  100% {
+    background-position: -100%, 0;
+  }
+}
+</style>
