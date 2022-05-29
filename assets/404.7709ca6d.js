c887e15beb5461e13146c55427e1164787797e1d
ataola <zjt613@gmail.com>
2022-05-29 11:23:30 +0000

    Deploying to gh-pages from  @ 8efe45f21b934417b5c139e29f973257a469c972 🚀

---
+++ assets/404.7709ca6d.js
@@ -0,0 +1,7 @@
+import{_}from"./index.724d626e.js";import{c as e,o as n}from"./vendor.db88fc1c.js";const r={};function c(o,t){return n(),e("pre",null,`  /\\/\\_____  _______      _____/\\/\\
+)/)/  |  | \\   _  \\    /  |  )/)/
+  /   |  |_/  /_\\  \\  /   |  |_
+ /    ^   /\\  \\_/   \\/    ^   /
+ \\____   |  \\_____  /\\____   |
+      |__|        \\/      |__|
+`)}var d=_(r,[["render",c]]);export{d as default};
