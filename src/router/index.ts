8efe45f21b934417b5c139e29f973257a469c972
ataola <zjt613@gmail.com>
2022-05-29 19:20:46 +0800

    🐞 fix:(*) rename NotFound

--- src/router/index.ts
+++ src/router/index.ts
@@ -100,7 +100,7 @@ const routes: Array<RouteRecordRaw> = [
   },
   {
     path: '/:pathMatch(.*)*',
-    name: 'not-found',
+    name: 'NotFound',
     component: () => import('@views/404.vue'),
     meta: {
       title: '404',
