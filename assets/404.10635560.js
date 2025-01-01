5146d7294cf384c43c66b27d15f89cbf563a89a8
ataola <zjt613@gmail.com>
2025-01-01 13:41:59 +0000

    Deploying to gh-pages from @ ataola/ataola.github.io@b60eb0a748b0232e63c40b36f730e31d0bc816a5 🚀

---
+++ assets/404.10635560.js
@@ -0,0 +1,7 @@
+import{_,o as e,c as n}from"./index.c66ee764.js";const r={};function c(t,a){return e(),n("pre",null,`  /\\/\\_____  _______      _____/\\/\\
+)/)/  |  | \\   _  \\    /  |  )/)/
+  /   |  |_/  /_\\  \\  /   |  |_
+ /    ^   /\\  \\_/   \\/    ^   /
+ \\____   |  \\_____  /\\____   |
+      |__|        \\/      |__|
+`)}var s=_(r,[["render",c]]);export{s as default};
